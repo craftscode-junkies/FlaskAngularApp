7cd7e67fd7fc79c5b56c937fa548092b276d0d2e
System Administrator <root@drjunks.home>
2019-09-18 17:46:00 +0200

    html bootstrap objects , Home component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Column, GridOption } from 'angular-slickgrid';
 
 @Component({
   selector: 'app-root',
@@ -6,18 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular Quiz';
-  const convertFile = () => {
-  const input = document.getElementById('fileInput');
+  columnDefinitions: Column[] = [];
+  gridOptions: GridOption = {};
+  dataset: any[] = [];
 
-  const reader = new FileReader();
-  reader.onload = () => {
-    let text = reader.result;
-    console.log('CSV: ', text.substring(0, 100) + '...');
+  ngOnInit(): void {
+    this.columnDefinitions = [
+      { id: 'title', name: 'Title', field: 'title', sortable: true },
+      { id: 'duration', name: 'Duration (days)', field: 'duration', sortable: true },
+      { id: '%', name: '% Complete', field: 'percentComplete', sortable: true },
+      { id: 'start', name: 'Start', field: 'start' },
+      { id: 'finish', name: 'Finish', field: 'finish' },
+      { id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven', sortable: true }
+    ];
+    this.gridOptions = {
+      enableAutoResize: true,       // true by default
+      enableCellNavigation: true
+    };
 
-    //convert text to json here
-    //var json = this.csvJSON(text);
-  };
-  reader.readAsText(input.files[0]);
-};
+    // fill the dataset with your data
+    // VERY IMPORTANT, Angular-Slickgrid uses Slickgrid DataView which REQUIRES a unique "id" and it has to be lowercase "id" and be part of the dataset
+    this.dataset = [];
+
+    // for demo purpose, let's mock a 1000 lines of data
+    for (let i = 0; i < 1000; i++) {
+      const randomYear = 2000 + Math.floor(Math.random() * 10);
+      const randomMonth = Math.floor(Math.random() * 11);
+      const randomDay = Math.floor((Math.random() * 28));
+      const randomPercent = Math.round(Math.random() * 100);
+
+      this.dataset[i] = {
+        id: i, // again VERY IMPORTANT to fill the "id" with unique values
+        title: 'Task ' + i,
+        duration: Math.round(Math.random() * 100) + '',
+        percentComplete: randomPercent,
+        start: `${randomMonth}/${randomDay}/${randomYear}`,
+        finish: `${randomMonth}/${randomDay}/${randomYear}`,
+        effortDriven: (i % 5 === 0)
+      };
+    }
+  }
 }
